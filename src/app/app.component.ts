13db61845beefc184743166b4d8fa6599e934031
TechnicalIllusion <robert_coones@yahoo.com>
2020-01-20 17:58:02 -0600

    completed assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  implements OnInit {
+  username = '';
+  message = '';
+
+  ngOnInit() {
+  }
+
+  onChangeSubmit() {
+    this.message = 'Welcome, ' + this.username + '!';
+    this.username = '';
+  }
+
+  onUpdateDataBound(event: Event) {
+    this.username = (event.target as HTMLInputElement).value;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
 import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, CommonModule],
   providers: [],
   bootstrap: [AppComponent]
 })
